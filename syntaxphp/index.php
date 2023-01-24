770f20920793a326a8e42087f4f3f3ba9225ebd7
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-25 00:00:34 +0700

    syntax php

---
+++ syntaxphp/index.php
@@ -0,0 +1,56 @@
+<?php 
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print => mencetak tulisan / isi variable
+// print_r => mencetak isi array
+// var_dump => debugging
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Nugie";
+// echo "Halo, nama saya $nama";
+
+// Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// Penggabungan string / concatenation / concat
+// .
+// $nama_depan = "Nugie";
+// $nama_belakang = "Jaya";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x += 5;
+// echo $x; // hasil akan menjadi 6
+// $nama = "Nugie";
+// $nama .= " ";
+// $nama .= "Jaya";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 < 5);   
+// var_dump(1 == "1"); // tidak mengecek tipe data
+
+// Identitas
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// $x = 10;
+// var_dump($x < 20 && $x % 2 == 0);
+
+?>
