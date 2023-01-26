25f35da0191c7f96c37678cc4c5a0271cd7620f9
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-01-26 21:03:50 +0700

    pengkondisian

--- 3-strukturkendali/latihan1.php
+++ 3-strukturkendali/latihan1.php
@@ -20,6 +20,8 @@
 //     echo "Hello World! <br>";
 //     $i++;
 // } while( $i < 10); 
+
+
 -->
 
 <!DOCTYPE html>
@@ -30,6 +32,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 1</title>
+    <style>
+        .warna-baris {
+            background: gray;
+        }
+    </style>
 </head>
 
 <body>
@@ -56,6 +63,23 @@
             </tr>
         <?php } ?>
     </table>
+    <br>
+
+    <!-- PENULISAN DENGAN MENGGANTI {} MENJADI :end | echo menjadi = -->
+    <table border="1" cellpadding="10" cellspacing="0">
+        <?php for ($i = 1; $i <= 5; $i++) : ?>
+            <?php if ($i % 2 == 1) : ?>
+                <tr class="warna-baris">
+            <?php else : ?>
+                <tr>
+            <?php endif;?>
+                <?php for ($j = 1; $j <= 5; $j++) : ?>
+                    <td> <?= "$i, $j" ?> </td>
+                <?php endfor; ?>
+            </tr>
+        <?php endfor; ?>
+    </table>
+
 </body>
 
 </html>
---
+++ 3-strukturkendali/latihan2.php
@@ -0,0 +1,19 @@
+<?php 
+// PENGKONDISIAN / PERCABANGAN
+// if else
+// if else if else
+// ternary
+// switch
+
+
+$x = 10;
+if ($x < 20) {
+    echo "benar";
+} else if ($x === 10){
+    echo "bingo";
+} else {
+    echo "salah";
+}
+
+
+?>
