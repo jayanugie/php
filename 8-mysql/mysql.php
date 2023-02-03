d32b85d8071db87016c4d598b650d7464447aec9
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-02-03 19:42:39 +0700

    setup mysql

---
+++ 8-mysql/mysql.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Database: phpdasar
+ * Table: Mahasiswa
+ * 
+ * Rows:
+ * -----
+ * id int primary key auto_increment
+ * nama varchar(100)
+ * nrp char(9)
+ * email varchar(100)
+ * jurusan varchar(100)
+ * gambar varchar(100)
+ * 
+ * 
+ */
+?>
