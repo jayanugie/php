f97157ae0e492baeda93f9cfbd3d3a8902504852
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-02-04 21:29:36 +0700

    insert data

---
+++ 10-insert&delete/functions.php
@@ -0,0 +1,37 @@
+<?php
+
+$connect = mysqli_connect("localhost", "root", "", "phpdasar");
+
+function query($query) {
+    global $connect;
+    $result = mysqli_query($connect, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc(($result))) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+
+function tambah($data) {
+    global $connect;
+
+    // ambil data dari tiap elemen form
+    $nrp = htmlspecialchars($data["nrp"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+    /**
+     * htmlspecialchars digunakan untuk menghindari user untuk 
+     * menulis dan mengaplikasikan tag html pada input.
+     */
+
+    // query insert data
+    $query = "INSERT INTO mahasiswa VALUES ('', '$nama', '$nrp', '$email', '$jurusan', '$gambar')";
+    mysqli_query($connect, $query);
+
+    return mysqli_affected_rows($connect);
+}
+
+?>
