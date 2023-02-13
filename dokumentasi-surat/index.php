9a80ebbd286aa249c42fb38d23a0d380af77b75c
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-02-13 09:47:21 +0700

    setup project

---
+++ dokumentasi-surat/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Surat</title>
+</head>
+<body>
+    <h1>Dokumentasi Surat</h1>
+    <a href="create.php">Tambah Surat</a>
+    <br><br>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>Tanggal Surat</th>
+            <th>Tanggal Entry</th>
+            <th>Jenis Surat</th>
+            <th>Asal Surat</th>
+            <th>Keterangan</th>
+            <th>Tag</th>
+            <th>Upload Surat</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Lorem</td>
+            <td>Lorem</td>
+            <td>Lorem</td>
+            <td>Lorem</td>
+            <td>Lorem</td>
+            <td>Lorem</td>
+        </tr>
+
+    </table>
+
+    
+</body>
+</html>
