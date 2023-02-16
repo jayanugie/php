99f286e19a4560d512d250f52922029630739119
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2023-02-16 14:53:55 +0700

    API edit, get by id

--- restfulapisurat/api/api_edit.php
+++ restfulapisurat/api/api_edit.php
@@ -1,42 +1,86 @@
 <?php 
-    // require_once('../config/koneksi_db.php');
-    // header('Content-Type: application/json; charset=utf8');
+    require_once('../config/koneksi_db.php');
+    header('Content-Type: application/json; charset=utf8');
 
-    // if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
 
-    //     $id = $_GET['id'];
-    //     $tanggal_surat = $_GET["tanggal_surat"];
-    //     $tanggal_entry = $_GET["tanggal_entry"];
-    //     $jenis_surat = $_GET["jenis_surat"];
-    //     $asal_surat = $_GET["asal_surat"];
-    //     $keterangan = $_GET["keterangan"];
-    //     $tag = $_GET["tag"];
-    //     $dokumen = $_GET["dokumen"];
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    //     $sql = "UPDATE surat SET 
-    //                 tanggal_surat = '$tanggal_surat', 
-    //                 tanggal_entry = '$tanggal_entry',
-    //                 jenis_surat = '$jenis_surat',
-    //                 asal_surat = '$asal_surat',
-    //                 keterangan = '$keterangan',
-    //                 tag = '$tag',
-    //                 dokumen = '$dokumen'
-    //             WHERE id = $id
-    //             ";
+        $id = $_POST['id'];
+        $tanggal_surat = $_POST['tanggal_surat'];
+        $tanggal_entry = $_POST['tanggal_entry'];
+        $jenis_surat = $_POST['jenis_surat'];
+        $asal_surat = $_POST['asal_surat'];
+        $keterangan = $_POST['keterangan'];
+        $tag = $_POST['tag'];
 
-    //     $query = mysqli_query($conn, $sql);
+        // Get file name of old file
+        $sql_select = "SELECT dokumen FROM surat WHERE id = $id";
+        $result = $conn->query($sql_select);
+        $row = $result->fetch_assoc();
+        $old_file_name = $row['dokumen'];
 
-    //     if ($query) {
-    //         $data = [
-    //             'status' => 'SUCCESS'
-    //         ];
-    //         echo json_encode([$data]);
-    //     } else {
-    //         $data = [
-    //             'status' => 'FAILED'
-    //         ];
-    //         echo json_encode([$data]);
-    //     }
-    // }
+
+            // DOKUMEN
+            $nama_file = $_FILES['dokumen']['name'];
+            $ukuran_file = $_FILES['dokumen']['size'];
+            $error = $_FILES['dokumen']['error'];
+            $tmp_name = $_FILES['dokumen']['tmp_name'];
+        
+            if ($error === 4) {
+                echo "PILIH DOKUMEN TERLEBIH DULU";
+                return false;
+            }
+        
+            $ekstensi_dokumen_valid = ['doc', 'docx', 'pdf', 'xls', 'xlsx', 'ppt', 'pptx'];
+            $ekstensi_dokumen = explode('.', $nama_file);
+            $ekstensi_dokumen = strtolower(end($ekstensi_dokumen));
+        
+            if (!in_array($ekstensi_dokumen, $ekstensi_dokumen_valid)) {
+                echo "YANG ANDA UPLOAD BUKAN DOKUMEN";
+                return false;
+            }
+        
+            if ($ukuran_file > 1500000) {
+                echo "UKURAN DOKUMEN TERLALU BESAR, MAKSIMAL 1.5MB";
+                return false;
+            }
+        
+            $nama_file_baru = uniqid();
+            $nama_file_baru .= '.';
+            $nama_file_baru .= $ekstensi_dokumen;
+
+            move_uploaded_file($tmp_name, '../files/'.$nama_file_baru);
+
+
+        $sql = "UPDATE surat SET 
+                    tanggal_surat = '$tanggal_surat',
+                    tanggal_entry = '$tanggal_entry',
+                    jenis_surat = '$jenis_surat',
+                    asal_surat = '$asal_surat',
+                    keterangan = '$keterangan',
+                    tag = '$tag',
+                    dokumen = '$nama_file_baru'
+                WHERE id = $id";
+
+        if ($conn->query($sql) === TRUE) {
+            unlink('../files/'.$old_file_name);
+            $response = array(
+                'status' => true,
+                'message' => 'Data berhasil diupdate'
+            );
+        } else {
+            $response = array(
+                'status' => false,
+                'message' => 'Error: ' . $sql . '<br>' . $conn->error
+            );
+        }
+    } else {
+        $response = array(
+            'status' => false,
+            'message' => 'Invalid Request'
+        );
+    }
+
+    echo json_encode($response);
 
 ?>
---
+++ restfulapisurat/api/api_tampil_byid.php
@@ -0,0 +1,19 @@
+<?php 
+    require_once('../config/koneksi_db.php');
+    header('Content-Type: application/json; charset=utf8');
+
+    $myArray = array();
+
+    if (isset($_GET["id"])) {
+        $id = $_GET["id"];
+
+        if ($result = mysqli_query($conn, "SELECT * FROM surat WHERE id= $id ")) {
+            while ($row = $result -> fetch_array(MYSQLI_ASSOC)) {
+                $myArray[] = $row;
+            }
+            mysqli_close($conn);
+            echo json_encode($myArray);
+        }
+    }
+
+?>
